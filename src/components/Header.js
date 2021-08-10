a1c0567738b74140425117e0d1741bca817c8ac6
neilburt <neil.burt@comcast.net>
2021-08-09 22:22:54 -0500

    Added component files and first page file, set-up some skeletal structure in said files.

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import Navigation from './Navigation'
+
+export default function Header(){
+  return (
+    <div>
+      <h1>Neil Burt</h1>
+      <Navigation />
+    </div>
+  )
+}
---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+export default function About (){
+  return (
+    <div>
+      <h1>A Bit About Me</h1>
+      <p>
+      I live in North Minneapolis with my girlfriend and cat companion. We have a beautiful house in the Victory neighborhood near Robbinsdale. I'm a gamer who loves to camp and go on walks. I love animals, am really into birding, and have always enjoyed dabbling with computers and tech.
+      </p>
+      <p>
+      I worked in retail for 13 years, the final three in management, primarily with stock and inventory control before deciding to move on. I made a natural transition to more warehouse work but in manufacturing instead and after a few years in that field, I decided it was time to make a change. Thus began my journey into the world of Coding.
+      </p>
+    </div>
+  )
+}
