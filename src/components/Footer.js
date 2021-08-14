da7e92759c95e04d00fd341559f670f5757d145a
neilburt <neil.burt@comcast.net>
2021-08-13 23:26:32 -0500

    Some adjustments, finished README.md except for deployed app link.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,13 +10,13 @@ export default function Footer() {
     <div className="container-fluid footer">
       <div className="row text-center">
         <div className="col">
-          <a href="https://github.com/neilburt">
+          <a href="https://github.com/neilburt" target="_blank" rel="noreferrer">
             <img className="footer-img github" alt="contact" src={github}></img>
           </a>
-          <a href="https://www.linkedin.com/in/neil-burt/">
+          <a href="https://www.linkedin.com/in/neil-burt/" target="_blank" rel="noreferrer">
             <img className="footer-img linkedin" alt="contact" src={linkedin}></img>
           </a>
-          <a href="https://www.facebook.com/neil.c.burt">
+          <a href="https://www.facebook.com/neil.c.burt" target="_blank" rel="noreferrer">
             <img className="footer-img facebook" alt="contact" src={facebook}></img>
           </a>
         </div>
