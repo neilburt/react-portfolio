e06d778350015b929316469d5fdd378eca2063f4
neilburt <neil.burt@comcast.net>
2021-08-16 13:45:31 -0500

    Tiny refactor.

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -11,7 +11,7 @@ export default function Contact() {
     const { target } = e;
     const inputType = target.name;
     const inputValue = target.value;
-    
+
     if (inputType === 'name') {
       setName(inputValue);
     } else if (inputType === 'email') {
@@ -36,7 +36,7 @@ export default function Contact() {
       setErrorMessage('Please enter a valid email.');
       return;
 
-    } else if (!name.length || !email.length || !message.length){
+    } else if (!name.length || !email.length || !message.length) {
       setErrorMessage('Please complete all fields.');
       return;
     }
@@ -50,52 +50,52 @@ export default function Contact() {
     <div className="container-fluid">
       <div className="row justify-content-center">
         <div className="col-6">
-      <form className="form">
-        <div className="form-group">
-        <label for="name">Name</label>
-        <input
-          value={name}
-          name="name"
-          onChange={handleInputChange}
-          onBlur={handleInputBlur}
-          className="form-control"
-          type="text"
-          placeholder="name"
-        />
+          <form className="form">
+            <div className="form-group">
+              <label for="name">Name</label>
+              <input
+                value={name}
+                name="name"
+                onChange={handleInputChange}
+                onBlur={handleInputBlur}
+                className="form-control"
+                type="text"
+                placeholder="name"
+              />
+            </div>
+            <div class="form-group">
+              <label for="email">Email</label>
+              <input
+                value={email}
+                name="email"
+                onChange={handleInputChange}
+                onBlur={handleInputBlur}
+                className="form-control"
+                type="email"
+                placeholder="email"
+              />
+            </div>
+            <div class="form-group">
+              <label for="message">Message</label>
+              <textarea
+                value={message}
+                name="message"
+                onChange={handleInputChange}
+                onBlur={handleInputBlur}
+                className="form-control"
+                rows="4"
+                type="text"
+                placeholder="your message">
+              </textarea>
+            </div>
+            <button type="button" className="btn" onClick={handleFormSubmit}>Submit</button>
+          </form>
+          {errorMessage && (
+            <div className="alert alert-warning" role="alert">
+              {errorMessage}
+            </div>
+          )}
         </div>
-        <div class="form-group">
-        <label for="email">Email</label>
-        <input
-          value={email}
-          name="email"
-          onChange={handleInputChange}
-          onBlur={handleInputBlur}
-          className="form-control"
-          type="email"
-          placeholder="email"
-        />
-        </div>
-        <div class="form-group">
-        <label for="message">Message</label>
-        <textarea
-          value={message}
-          name="message"
-          onChange={handleInputChange}
-          onBlur={handleInputBlur}
-          className="form-control"
-          rows="4"
-          type="text"
-          placeholder="your message">
-        </textarea>
-        </div>
-        <button type="button" className="btn" onClick={handleFormSubmit}>Submit</button>
-      </form>
-      {errorMessage && (
-        <div className="alert alert-warning" role="alert">
-          {errorMessage}
-        </div>
-      )}
-      </div>
       </div>
     </div>
   )
