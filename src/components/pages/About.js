4c27b08eeb310620d2ba5e95862db382f30984ee
Neil Burt <neil.burt@comcast.net>
2021-09-21 16:10:18 -0500

    Changed some verbage on About.

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -11,7 +11,7 @@ export default function About() {
           I am a junior software engineer leveraging leadership, organizational, and time-management skills gained over more than 15 years of experience in warehouse, inventory, and team management. I am highly experienced in meeting goals and deadlines individually and as part of a team. I have been commended by supervisors for my ability to adapt to ever-changing work environments and go above and beyond to meet daily, as well as extraordinary, demands. I have been recognized for resourcefulness, flexibility, problem-solving skills, high attention to detail, and my ability to accomplish tasks with minimal instruction and supervision.
           </p>
           <p>
-          I have recently completed the rigorous web development certificate program through the University of Minnesota and am very eager to maintain this momentum into the field, contributing work ethic, loyalty, and extensive experience in collaboration and customer satisfaction to the success of a business.
+          I have recently completed the rigorous web development certificate program through the University of Minnesota and am excited to continue learning and eager to maintain this momentum into the field, contributing work ethic, loyalty, and extensive experience in collaboration and customer satisfaction to the success of a business.
           </p>
           <p>
           I live in North Minneapolis with my partner and our cat companion. We have a beautiful house in the Victory neighborhood near Robbinsdale. I'm a gamer who loves to camp and go on walks. I love animals, am really into birding, and have always enjoyed dabbling with computers and tech.
